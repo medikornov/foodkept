a7169997539af756f3f3029ae6846ac4f4bceddb
Kaveckas Vasaris <vasaris.kaveckas@mif.stud.vu.lt>
2021-09-21 11:35:59 +0300

    Add login and register pages with working DB

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,10 @@
+using FoodKept.Data;
+using FoodKept.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,8 +27,27 @@ namespace FoodKept
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-        }
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            services.AddDbContext<FoodContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("FoodContext")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<FoodContext>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequiredUniqueChars = 0;
+            }
+            );
+            services.ConfigureApplicationCookie(config =>
+                config.LoginPath = "/Login"
+            );
+         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,6 +55,7 @@ namespace FoodKept
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
@@ -45,6 +69,7 @@ namespace FoodKept
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
