2e0853115b42c35881792bd1de84529d8293794e
arma155 <78311210+ArmantasPik@users.noreply.github.com>
2021-10-13 11:13:38 +0300

    Added food categories

--- ViewModels/FoodCategories.cs
+++ ViewModels/FoodCategories.cs
@@ -5,12 +5,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FoodKept.ViewModels
 {
-    public class EditLatLng
+    public class FoodCategories
     {
-        public double Lat { get; set; }
-        public double Lng { get; set; }
+        public enum Category
+        {
+            Fruit,
+            FastFood,
+            Pastries
+        }
     }
 }
