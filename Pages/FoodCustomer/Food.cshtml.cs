059204beba864b67d16172b2e2ae07e354b8c297
arma155 <78311210+ArmantasPik@users.noreply.github.com>
2021-11-09 23:34:31 +0200

    Add sorting to customer food page

--- Pages/FoodCustomer/Food.cshtml.cs
+++ Pages/FoodCustomer/Food.cshtml.cs
@@ -28,29 +28,70 @@ namespace FoodKept.Pages
         public string id { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
+        public string NameSort { get; set; }
+        public string RestaurantSort { get; set; }
+        public string PriceSort { get; set; }
+        public string DiscountSort { get; set; }
         public string QuantitySort { get; set; }
+        public string FoodCategorySort { get; set; }
 
         public async Task OnGetAsync(string sortOrder)
         {
             //Lazy Initialization
-            Lazy<List<Food>> getFood = new Lazy<List<Food>>(() => _context.FoodData.ToList());
+            //Lazy<List<Food>> getFood = new Lazy<List<Food>>(() => _context.FoodData.ToList());
 
-            //Sorting by quantity
+            //Sorting system
+            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            RestaurantSort = string.IsNullOrEmpty(sortOrder) ? "resName_desc" : "Restaurant";
+            PriceSort = sortOrder == "Price" ? "price_desc" : "Price";
+            DiscountSort = sortOrder == "Discount" ? "discount_desc" : "Discount";
             QuantitySort = sortOrder == "Quantity" ? "quantity_desc" : "Quantity";
+            FoodCategorySort = string.IsNullOrEmpty(sortOrder) ? "category_desc" : "Category";
 
-            switch(sortOrder)
+            IQueryable<Food> foodIQ = from s in _context.FoodData
+                                             select s;
+
+            switch (sortOrder)
             {
+                case "name_desc":
+                    foodIQ = foodIQ.OrderByDescending(s => s.FoodName);
+                    break;
+                case "Restaurant":
+                    foodIQ = foodIQ.OrderBy(s => s.ApplicationUser.RestaurantName);
+                    break;
+                case "resName_desc":
+                    foodIQ = foodIQ.OrderByDescending(s => s.ApplicationUser.RestaurantName);
+                    break;
+                case "Price":
+                    foodIQ = foodIQ.OrderBy(s => s.Price);
+                    break;
+                case "price_desc":
+                    foodIQ = foodIQ.OrderByDescending(s => s.Price);
+                    break;
+                case "Discount":
+                    foodIQ = foodIQ.OrderBy(s => s.CurrentPrice.DiscountPercent);
+                    break;
+                case "discount_desc":
+                    foodIQ = foodIQ.OrderByDescending(s => s.CurrentPrice.DiscountPercent);
+                    break;
                 case "Quantity":
-                    Food = getFood.Value;
+                    foodIQ = foodIQ.OrderBy(s => s.Quantity);
                     break;
                 case "quantity_desc":
-                    Food = getFood.Value;
-                    Food.Sort();
+                    foodIQ = foodIQ.OrderByDescending(s => s.Quantity);
+                    break;
+                case "Category":
+                    foodIQ = foodIQ.OrderBy(s => s.FoodCategory);
+                    break;
+                case "category_desc":
+                    foodIQ = foodIQ.OrderByDescending(s => s.FoodCategory);
                     break;
                 default:
-                    Food = getFood.Value;
+                    foodIQ = foodIQ.OrderBy(s => s.FoodName);
                     break;
             }
+
+            Food = await foodIQ.ToListAsync();
 
             //Calculate Discounts
             CalculateCurrentPrice.CalculatePriceForFoodList(Food: Food);
