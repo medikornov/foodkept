c145102121e347de6e744ad4327d8ec6c44c00be
0rl1s <lukasorliukas123@gmail.com>
2021-11-09 20:54:53 +0200

    Finished not logged feedback form

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -11,8 +11,10 @@ namespace FoodKept.Models
     public class Feedback
     {
         public int ID { get; set; }
+        public int Points { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string RestaurantName { get; set; }
         public string Email { get; set; }
         public string Answer { get; set; }
     }
